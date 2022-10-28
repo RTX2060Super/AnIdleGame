7cef6a6942fa6522b069e574a1c4df4d2f8472d6
aRandomN3rd <104280607+aRandomN3rd@users.noreply.github.com>
2022-10-27 22:21:52 -0500

    its broken
    it doesnt work but ill fix later

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-var clicks = 0
+var money = 10
+var generator = []
 
-function clix() {
-    clicks +=1
-    document.getElementById("clicks").textContent = "Clicks: " + clicks
-
+for (let i = 0; i < 10; i++) {
+    let generator = {
+        cost: Math.pow(Math.pow(10, i), i),
+        bought: 0,
+        amount: 0,
+        mult: 1,
+    }
+    generators.push(generator)
 }
